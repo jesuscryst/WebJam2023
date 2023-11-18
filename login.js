8249a079435a5ca0d53f33c582ab59f0e061914e
realisticdatabase <149647912+jesuscryst@users.noreply.github.com>
2023-11-17 16:07:00 -0800

    added creating an account function

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 var users = [
-    { username: ['petrzot', 'lucyliu'] , password: ['zotzot', 'mewmew'] },
+    { username: ['petrzot'] , password: ['zotzot'] , email: ['zotastic@gmail.com']},
 ];
 
 function submitLogin() {
@@ -7,7 +7,7 @@ function submitLogin() {
     const password = document.getElementById('password').value;
 
     const user = users.find(userData =>
-        userData.username.includes(username) && userData.password.includes(password)
+        (((userData.username.includes(username) && userData.password.includes(password))) || (userData.email.includes(username) && userData.password.includes(password)))
     );
 
     if (user) {
@@ -17,6 +17,23 @@ function submitLogin() {
     }
 }
 
-function createAccount() {
+function addToUsers(key, input) {
+    const user = users[0];
+    if (user.hasOwnProperty(key)) {
+        user[key].push(input.value);
+    } else {
+        console.error('Invalid key:', key);
+    }
+}
 
+function createAccount() {
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    addToUsers('username', username);
+    addToUsers('email', email);
+    addToUsers('password', password);
+
+    window.location.href = 'index.html';
 }
